574712a4cd087f239e2db13c41c009a97c52e7f5
alex <alex.garulli@kurtgeiger.com>
2016-05-22 03:31:36 +0100

    commit all files

--- dev/app/campers/campers.controller.js
+++ dev/app/campers/campers.controller.js
@@ -7,17 +7,20 @@
     // THE NAME OF THE FIRST CAMPERS SHOULD BE "WORKING FROM LOCAL JSON"
     // IN shared/service.js YOU HAVE TO UNCOMMENT THE RIGHT SERVICE  and main.controller.js YOU HAVE TO UNCOMMENT THE RIGHT BODY CONTROLLER
 
-    /*
+ 
       getData.async().then(function(data) {
+          $scope.debug = "You are now working retriving data from local JSON"
           $scope.campersData = data["FccLond-data"]["campers"]
       });
-    */
+      
 
     // CONTROLLER BODY FOR AJAX CALL TO FIREBASE
     // COMMENT THIS OUT IF YOU WANT TO WORK WITH LOCAL JSON
     // THE NAME OF THE FIRST CAMPERS SHOULD BE "WORKING FROM FIREBASE"
-    getData.async().then(function(data) {
+     /*getData.async().then(function(data) {
+      $scope.debug = "You are now working retriving data from Firebase";      
       $scope.campersData = data.campers;
-    })
+    })*/
   }
 })();
+
