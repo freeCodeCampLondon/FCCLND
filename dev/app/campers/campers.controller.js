c0f8935d6c0fb099d19218ba4cb903cba5db8a79
alex <alex.garulli@kurtgeiger.com>
2016-05-24 22:24:33 +0100

    merge master into branch and clear conflict

--- dev/app/campers/campers.controller.js
+++ dev/app/campers/campers.controller.js
@@ -6,19 +6,17 @@
     // CONTROLLER BODY FOR AJAX CALL TO LOCAL JSON - UNCOMMENT THIS IF YOU WANT TO WORK WITH LOCAL JSON
     // THE NAME OF THE FIRST CAMPERS SHOULD BE "WORKING FROM LOCAL JSON"
     // IN shared/service.js YOU HAVE TO UNCOMMENT THE RIGHT SERVICE  and main.controller.js YOU HAVE TO UNCOMMENT THE RIGHT BODY CONTROLLER
-
- 
-     /* getData.async().then(function(data) {
+      getData.async().then(function(data) {
+          $scope.debug = "Working from LOCAL JSON"
           $scope.campersData = data["FccLond-data"]["campers"]
       });
-      */
-
     // CONTROLLER BODY FOR AJAX CALL TO FIREBASE
     // COMMENT THIS OUT IF YOU WANT TO WORK WITH LOCAL JSON
     // THE NAME OF THE FIRST CAMPERS SHOULD BE "WORKING FROM FIREBASE"
-     getData.async().then(function(data) {
-      $scope.campersData = data.campers;
-    })
+    /* getData.async().then(function(data) {
+        $scope.debug = "Working from LOCAL FIREBASE"
+        $scope.campersData = data.campers;
+    })*/
   }
 })();
 
