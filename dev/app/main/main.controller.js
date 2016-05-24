d3428edd9d7d5fcd3db60af96f306c3912847a33
alex <alex.garulli@kurtgeiger.com>
2016-05-24 22:46:54 +0100

    update master so read from local

--- dev/app/main/main.controller.js
+++ dev/app/main/main.controller.js
@@ -9,11 +9,11 @@
     // THE NAME OF THE FIRST CAMPERS SHOULD BE "WORKING FROM LOCAL JSON"
     // IN shared/service.js YOU HAVE TO UNCOMMENT THE RIGHT SERVICE and campers.controller.js YOU HAVE TO UNCOMMENT THE RIGHT BODY CONTROLLER
 
-    /*
+    
       getData.async().then(function(data) {
         $scope.mainData = data["FccLond-data"]["main"][0];
        });
-    */
+    
     
 
     // CONTROLLER BODY FOR AJAX CALL TO FIREBASE
@@ -21,9 +21,9 @@
     // NAME OF THE FIRST CAMPERS SHOULD BE "WORKING FROM FIREBASE"
     
 
-     getData.async().then(function(data) {
+   /*  getData.async().then(function(data) {
       $scope.mainData = data.main[0];
-    })
+    })*/
   }
 
 })();
