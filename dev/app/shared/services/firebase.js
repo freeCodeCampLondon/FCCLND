98478ac0aff9ec88eaa18ce45cb480393f094a3b
alex <alex.garulli@kurtgeiger.com>
2016-06-14 22:00:45 +0100

    same json for local and fibase

--- dev/app/shared/services/firebase.js
+++ dev/app/shared/services/firebase.js
@@ -5,7 +5,7 @@ var Firebase = require('firebase');
 module.exports = ['$firebaseObject', function ($firebaseObject) {
   var getData = {
     async: function () {
-      var ref = new Firebase('https://glowing-fire-4904.firebaseio.com/FccLond-data/');
+      var ref = new Firebase('https://glowing-fire-4904.firebaseio.com/');
       var obj = new $firebaseObject(ref).$loaded().then(function (res) {
         return res;
       });
