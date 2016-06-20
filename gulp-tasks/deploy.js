dd4dc7993efd4fdd54dc298c3fc97104b326306e
alex <alex.garulli@gmail.com>
2016-06-20 17:25:26 +0100

    gulp if

--- gulp-tasks/deploy.js
+++ gulp-tasks/deploy.js
@@ -6,11 +6,11 @@ var $ = require('gulp-load-plugins')();
 gulp.task('deploy', ['build'], function () {
   return gulp.src('./dist/app/**/*')
     /* This is to ensure that we work from Firebase in production */
-    .pipe(gulpif('*.js', $.replace('var localData = true;', 'var localData = false;')))
-    .pipe(gulpif('*.js', $.uglify({ mangle: false }))) // Minify JavaScript
-    .pipe(gulpif('*.js', $.rev()))
-    .pipe(gulpif('*.css', $.cleanCss())) // Minify CSS
-    .pipe(gulpif('*.css', $.rev()))
+    .pipe($.if('*.js', $.replace('var localData = true;', 'var localData = false;')))
+    .pipe($.if('*.js', $.uglify({ mangle: false }))) // Minify JavaScript
+    .pipe($.if('*.js', $.rev()))
+    .pipe($.if('*.css', $.cleanCss())) // Minify CSS
+    .pipe($.if('*.css', $.rev()))
     .pipe($.revReplace())
     .pipe($.deploy());
 });
