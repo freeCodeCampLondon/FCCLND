f560cdd8bca79f9f174be1b16b7146a7c0fddffc
alex <alex.garulli@gmail.com>
2016-06-20 22:01:57 +0100

    conflict

--- dev/app/campers/campers.directive.js
+++ dev/app/campers/campers.directive.js
@@ -91,6 +91,13 @@ module.exports = function isoRepeat($timeout, $window) {
         });
       };
 
+
+      $scope.filterSupporter = function () {
+         $scope.iso.arrange({
+          filter: '.fcc-lnd-supporter'
+        });
+      };
+
       // SECOND LEVEL FE
       $scope.filterHtml = function () {
         $scope.iso.arrange({
