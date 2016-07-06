6aae1733c393e43b005796cafa55b73fab956985
Chris Alexander <git@chrisalexander.me.uk>
2016-07-06 20:55:30 +0100

    remove bower reference in linting gulp task

--- gulp-tasks/script.js
+++ gulp-tasks/script.js
@@ -4,7 +4,7 @@ var $ = require('gulp-load-plugins')();
  * Lint JavaScript
  */
 gulp.task('lint', function() {
-  return gulp.src(['./**/*.js', '!node_modules/**', '!bower_components/**', '!dist/**'])
+  return gulp.src(['./**/*.js', '!node_modules/**', '!dist/**'])
     .pipe($.eslint())
     .pipe($.eslint.format());
 });
