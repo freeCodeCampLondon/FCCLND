0926d39c6b5e51330b2eb122b96c349f91fe469c
Chris Alexander <git@chrisalexander.me.uk>
2016-09-14 20:07:35 +0100

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var NODE_ENV = process.env.NODE_ENV || 'development';
 
+var path = require('path');
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
@@ -10,13 +11,13 @@ var isDev = NODE_ENV === 'development';
 var isProd = NODE_ENV === 'production';
 
 var config = {
-  context: __dirname + '/dev/app',
+  context: path.join(__dirname, '/dev/app'),
   entry: [
     './app.js'
   ],
   output: {
-    path: __dirname + '/dist',
-    filename: 'bundle.js'
+    path: path.join(__dirname + '/dist'),
+    filename: '[hash].bundle.js'
   },
   resolve: {
     alias: {
@@ -44,11 +45,11 @@ var config = {
       },
       { /* WOFF Fonts */
         test: /\.(woff|woff2)(\?[\s\S]+)?$/,
-        loader: 'url?mimetype=application/font-woff&name=assets/fonts/[name].[hash].[ext]'
+        loader: 'url?mimetype=application/font-woff&name=assets/fonts/[hash].[name].[ext]'
       },
       { /* TTF, EOT & SVG Fonts */
         test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
-        loader: 'file?name=assets/fonts/[name].[hash].[ext]'
+        loader: 'file?name=assets/fonts/[hash].[name].[ext]'
       }
     ]
   },
@@ -57,11 +58,11 @@ var config = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: __dirname + '/dev/app/index.html',
+      template: path.join(__dirname, '/dev/app/index.html'),
       filename: 'index.html',
       inject: 'body'
     }),
-    new ExtractTextPlugin('main.css'),
+    new ExtractTextPlugin('[contenthash].[name].css'),
     new CopyWebpackPlugin([
       {
         from: 'data/fccLndData.json',
@@ -71,10 +72,22 @@ var config = {
         from: 'assets/images',
         to: 'assets/images'
       }
-    ]),
-    new CleanWebpackPlugin(['dist'])
+    ])
   ]
 };
+
+/**
+ * Development Config Options
+ **/
+ if(isDev) {
+   config.devtool = 'inline-source-map';
+   config.module.loaders = [
+     { /* Expose React to devtools */
+       test: require.resolve('react'),
+       loader: 'expose?React'
+     }
+   ].concat(config.module.loaders);
+ }
 
 /**
  * Production Config Options
@@ -89,6 +102,21 @@ if(isProd) {
       replace: 'false'
     }
   });
+  /* Optimise Output */
+  config.plugins = [
+    new CleanWebpackPlugin(['dist']),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+  ].concat(config.plugins);
 }
 
 module.exports = config;
